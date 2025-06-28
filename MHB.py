ac55a7666baef6230039555b655b99a350260062
MR-MUNA-404 <136841898+MR-MUNA-404@users.noreply.github.com>
2025-06-28 21:49:20 +0500

    Add files via upload

---
+++ MHB.py
@@ -0,0 +1,10 @@
+#-
+import os
+print("Join my Whatsapp Group")
+os.system('xdg-open https://chat.whatsapp.com/HdppaiQyFtZ1AwiOzc5iEe')
+import os
+os.system('git pull')
+import MHB
+
+
+               
